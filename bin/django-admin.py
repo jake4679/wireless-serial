4349c993c4dfb584e918a7369f0d57e06fd5a992
Jake Cheuvront <jcheuvront@irobot.com>
2014-08-23 12:33:11 -0400

    Initial commit of  python virtual environment

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jcheuvront/wireless-serial/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.6.6','django-admin.py'
+__requires__ = 'Django==1.6.6'
+import pkg_resources
+pkg_resources.run_script('Django==1.6.6', 'django-admin.py')
